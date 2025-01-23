f5e18cd2343bf66f2a299616c119ea5716111716
SkyTab4096 <korya4096@gmail.com>
2025-01-23 10:06:08 -0700

    M02 Project1

--- Assignment1P3StartFile.py
+++ Assignment1P3StartFile.py
@@ -2,7 +2,7 @@
 #Class: INFO 1200
 #Section: 001
 #Professor: Sharp
-#Date: 1/21/25
+#Date: 1/23/25
 #Project #: 2
 #By submitting this assignment, I declare that the source code contained in this assignment was written #solely by me, unless specifically provided in the assignment. I attest that no part of this assignment, #in whole or in part, was directly created by Generative AI, unless explicitly stated in the assignment #instructions, nor obtained from a subscription service. I understand that copying any source code, #in whole or in part, unless specifically provided in the assignment, constitutes cheating, and that #I will receive a zero on this project if I am found in violation of this policy.
 firstName = 'Kory' #My First Name
