05a68d3a1461580190c3880fddc5a5d560e0dc12
SkyTab4096 <korya4096@gmail.com>
2025-02-14 10:05:24 -0700

    M04 Project 1 Date Changes

--- AndersonKory_MO4P1.py
+++ AndersonKory_MO4P1.py
@@ -2,7 +2,7 @@
 #Class: INFO 1200
 #Section: 001
 #Professor: Sharp
-#Date: 2/13/25
+#Date: 2/14/25
 #Assignment #: Project 4
 #By submitting this assignment, I declare that the source code contained in this assignment was written #solely by me, unless specifically provided in the assignment. I attest that no part of this assignment, #in whole or in part, was directly created by Generative AI, unless explicitly stated in the assignment #instructions, nor obtained from a subscription service. I understand that copying any source code, #in whole or in part, unless specifically provided in the assignment, constitutes cheating, and that #I will receive a zero on this project if I am found in violation of this policy.
 
--- AndersonKory_MO4P2.py
+++ AndersonKory_MO4P2.py
@@ -2,7 +2,7 @@
 #Class: INFO 1200
 #Section: 001
 #Professor: Sharp
-#Date: 2/13/25
+#Date: 2/14/25
 #Assignment #: Project 4
 #By submitting this assignment, I declare that the source code contained in this assignment was written #solely by me, unless specifically provided in the assignment. I attest that no part of this assignment, #in whole or in part, was directly created by Generative AI, unless explicitly stated in the assignment #instructions, nor obtained from a subscription service. I understand that copying any source code, #in whole or in part, unless specifically provided in the assignment, constitutes cheating, and that #I will receive a zero on this project if I am found in violation of this policy.
 
@@ -12,6 +12,7 @@ print() # Prints a blank line
 mealCost = float(input("Cost of Meal: ")) # Asks and stores the cost of the meal
 print() # Prints a blank line
 
+# The following code does not use a for loop because a for loop requires using either a list or an array.
 #i = 0 # Sets an index counter
 #while i < 3: # Only lets the loop run three times
 #    if i == 0: # First iteration of the loop
@@ -40,6 +41,7 @@ print() # Prints a blank line
 #        print(f"Total Amount: {total}") # Prints the total cost
 #    i += 1 # Incriments the index counter
 
+# The following code utilizes a for loop but also has an array.
 for tip in [15, 20, 25]: # Runs the following code for each tip portion
     print(f"{tip}%") # Prints the tip percentage
     tipPercent = tip / 100 # Calculates the tip percent
