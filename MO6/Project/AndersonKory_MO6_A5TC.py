5be2008c36000ac9b8b69d1f020c1a8beaef6574
SkyTab4096 <korya4096@gmail.com>
2025-03-24 10:33:03 -0600

    Added pseudo-code agreement to each python file for MO6 Project

--- MO6/Project/AndersonKory_MO6_A5TC.py
+++ MO6/Project/AndersonKory_MO6_A5TC.py
@@ -1,5 +1,13 @@
 #!/usr/bin/env python3
 
+#Name: Kory Anderson
+#Class: INFO 1200
+#Section: 001
+#Professor: Sharp
+#Date: March 24, 2025
+#Assignment #: 6
+#By submitting this assignment, I declare that the source code contained in this assignment was written #solely by me, unless specifically provided in the assignment. I attest that no part of this assignment, #in whole or in part, was directly created by Generative AI, unless explicitly stated in the assignment #instructions, nor obtained from a subscription service. I understand that copying any source code, #in whole or in part, unless specifically provided in the assignment, constitutes cheating, and that #I will receive a zero on this project if I am found in violation of this policy.
+
 tax = 0.06 # Sets a standard tax rate
 
 def sales_tax(total): # Defines a function to calculate sales tax
