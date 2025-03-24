014e78900552beb23579cda874b2cf34d88b78c4
Kory <korya4096@gmail.com>
2025-03-24 15:42:49 -0600

    MO7 Participation

--- MO7/Participation/calculator.py
+++ MO7/Participation/calculator.py
@@ -3,17 +3,9 @@ from dataclasses import dataclass
 @dataclass
 class calculation():
     diameter:float = 0.0
-    circumference:float = 0.0
-
+    
     def calculate(self):
-        if self.diameter == 0 and self.circumference != 0:
-            pi = float(3.14159265359)
-            radius = self.circumference / pi / 2
-            diameter = 2 * radius
-
-            return diameter
-        
-        elif self.diameter != 0 and self.circumference == 0:
+        if self.diameter != 0:
             pi = float(3.14159265359)
             radius = self.diameter / 2
             circumference = 2 * pi * radius
@@ -21,4 +13,4 @@ class calculation():
             return circumference
         
         else:
-            self.message += f"Diameter or Circumference must not be empty"
+            self.message += f"Diameter must not be empty"
