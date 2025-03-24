6f8056ad036e0b012d29506def43cf69aa7cf42b
SkyTab4096 <korya4096@gmail.com>
2025-03-24 09:59:53 -0600

    MO6 Participation

--- MO6/Participation/future_value-3-revised.py
+++ MO6/Participation/future_value-3-revised.py
@@ -1,35 +1,33 @@
 #!/usr/bin/env python3
         
 def get_float(prompt, low, high):
-    while True:
-        number = float(input(prompt))
+    is_valid = True
+    while is_valid == True:
+        number = float(prompt)
         if number > low and number <= high:
             is_valid = True
             return number
         else:
-            print("Entry must be greater than", low,
-                  "and less than or equal to", high,
-                  "Please try again.")
+            print(f"Entry must be greater than {low} \n and less than or equal to {high}\n Please try again.")
             
 def get_integer(prompt, low, high):
-    while True:
-        number = int(input(prompt))
+    is_valid = True
+    while is_valid == True:
+        number = int(prompt)
         if number > low and number <= high:
             is_valid = True
             return number
         else:
-            print("Entry must be greater than", low,
-                  "and less than or equal to", high,
-                  "Please try again.")
+            print(f"Entry must be greater than {low} \n and less than or equal to {high}\n Please try again.")
             
-def calculate_future_value(monthly_investment, yearly_interest, years):
+def calculate_future_value(monthly_investment, yearly_interest_rate, years):
     # convert yearly values to monthly values
-    monthly_interest_rate = yearly_interest / 12
+    monthly_interest_rate = yearly_interest_rate / 100 / 12
     months = years * 12
 
     # calculate future value
-    future_value = 0.0
-    for i in range(1, months):
+    future_value = 0
+    for i in range(0, months):
         future_value += monthly_investment
         monthly_interest = future_value * monthly_interest_rate
         future_value += monthly_interest
@@ -40,16 +38,16 @@ def main():
     choice = "y"
     while choice.lower() == "y":
         # get input from the user
-        monthly_investment = get_float("Enter monthly investment:\t", 0, 1000)
-        yearly_interest_rate = get_float("Enter yearly interest rate:\t", 0, 15)
-        years = get_integer("Enter number of years:\t\t", 0, 50)
+        monthly_investment = get_float(input("Enter monthly investment:\t\t"), 0, 1000)
+        yearly_interest_rate = get_float(input("Enter yearly interest rate in %:\t"), 0, 100)
+        years = get_integer(input("Enter number of years:\t\t\t"), 0, 50)
 
         # get and display future value
         future_value = calculate_future_value(
             monthly_investment, yearly_interest_rate, years)
         
         print()
-        print(f"Future value:\t\t\t{round(future_value, 2)}")
+        print(f"Future value:\t\t\t\t{round(future_value, 2)}")
         print()
 
         # see if the user wants to continue
