e189e869e95374b09456ac1e51a7213f0a2f336d
SkyTab4096 <korya4096@gmail.com>
2025-04-02 14:24:33 -0600

    MO8 Project

--- MO8/Participation/AndersonKory_test_scores-3.py
+++ MO8/Participation/AndersonKory_test_scores-3.py
@@ -1,70 +1,69 @@
 #!/usr/bin/env python3
 
-def display_welcome():
-    print("The Test Scores program")
-    print("Enter 'x' to exit")
-    print("")
+def display_welcome(): # Defines a function to display a welcome message
+    print("The Test Scores program") # Prints the title of the application
+    print("Enter 'x' to exit") # Defines how to exit the program
+    print("") # Prints a blank line
 
-def get_scores():
-    scoreList = []
-    while True:
-        score = input("Enter test score: ")
-        if score == "x":
-            scoreList.sort()
-            return  scoreList
-        else:
-            score = int(score)
-            if score >= 0 and score <= 100:
-                scoreList.append(score)
-            else:
-                print("Test score must be from 0 through 100. " +
-                      "Score discarded. Try again.")
+def get_scores(): # Defines a function to get the test scores from the user
+    scoreList = [] # Makes an empty array for the test scores
+    while True: # Makes a loop for entering test scores
+        score = input("Enter test score: ") # Gets a test score from the user and stores it as a variable
+        if score == "x": # If the entered value is the exit character run the following
+            scoreList.sort() # Sort the list of test scores
+            return  scoreList # Returns the sorted list to the program that called this function
+        else: # If the exit character was not entered run the following
+            score = getInt(score) # Calls a function to verify the score is an integer
+            if score >= 0 and score <= 100: # Verifies that the score is within the boundaries
+                scoreList.append(score) # Adds the score to the score array
+            else: # If the score is not within the specified range
+                print("Test score must be from 0 through 100.\nScore discarded. Try again.") # Prints an error message
 
-def process_scores(scoreList):
-    # calculate average score
-    length = len(scoreList)
-    scoreTotal = 0
-    medianScores = []
-    for i in scoreList:
-        scoreTotal += i
-    averageScore = round(scoreTotal / length, 0)
-    minScore = min(scoreList)
-    maxScore = max(scoreList)
-    medianIndex = length / 2
-    if medianIndex % 2 != 0:
-        medianIndex = int(medianIndex - .5)
-        medianScore = scoreList[medianIndex]
-    else:
-        medianIndex1 = int(medianIndex - .5)
-        medianIndex2 = int(medianIndex + .5)
-        medianScores.append(scoreList[medianIndex1])
-        medianScores.append(scoreList[medianIndex2])
+def getInt(score): # Defines a function to verify that the entered test score is an integer
+    try: # When called try running the following code
+        return int(score) # Trys to return the passed score as an integer
+    except ValueError: # If there was an error when converting the score into an integer, run the following
+        print("Score entered is not an integer.\nScore discarded.  Please try again.") # Prints an error message
 
-    # format and display the result
-    print()
-    print(f"Score total:\t\t{scoreTotal}")
-    print(f"Number of Scores:\t{length}")
-    print(f"Average Score:\t\t{averageScore}")
-    print(f"Minimum Score:\t\t{minScore}")
-    print(f"Maximum Score:\t\t{maxScore}")
-    if len(medianScores) == 0:
-        print(f"Median Score:\t\t{medianScore}")
-    else:
-        intialString = str(medianScores)
-        partialString = intialString.replace("[", "")
-        medianScoresString = partialString.replace("]", "")
-        print(f"Median Scores:\t\t{medianScoresString}")
+def process_scores(scoreList): # Defines a function to process the list of scores
+    length = len(scoreList) # Stores the length of the list
+    scoreTotal = 0 # Initializes the total score as 0
+    medianScores = [] # Initializes a new list to store the median scores
+    for i in scoreList: # For every value in the score list, 
+        scoreTotal += i # Add that value to the total score
+    averageScore = round(scoreTotal / length, 0) # Calculates the average score, rounding to the nearest integer
+    minScore = min(scoreList) # Finds the minimum score
+    maxScore = max(scoreList) # Finds the maximum score
+    medianIndex = length / 2 # Finds the middle number in the score list
+    if medianIndex % 2 != 0: # If the median index is not even run the following
+        medianIndex = getInt(medianIndex - .5) # Restore the median index value as an integer 
+        medianScore = scoreList[medianIndex] # Grabs the score in the score list at the median index value
+    else: # If the median index is odd, run the following
+        medianIndex1 = int(medianIndex - .5) # Stores a new median index, that is the next lowest integer from the median index value
+        medianIndex2 = int(medianIndex + .5) # Stores a new median index, that is the next highest integer from the median index value
+        medianScores.append(scoreList[medianIndex1]) # Stores the first median score in a list of median scores
+        medianScores.append(scoreList[medianIndex2]) # Stores the second median score in a list of median scores
+        medianScores.sort() # Sorts the list of median scores
 
+    print() # Prints a blank line
+    print(f"Score total:\t\t{scoreTotal}") # Displays the total score
+    print(f"Number of Scores:\t{length}") # Displays the length of the score list
+    print(f"Average Score:\t\t{averageScore}") # Displays the calculated average score
+    print(f"Minimum Score:\t\t{minScore}") # Displays the minimum entered score
+    print(f"Maximum Score:\t\t{maxScore}") # Displays the maximum entered score
+    if len(medianScores) == 0: # Checks to see if a value was added to the median scores list
+        print(f"Median Score:\t\t{medianScore}") # Prints the singular median score, if nothing was added to the median scores list
+    else: # If a value was added to the median scores list, run the following
+        intialString = str(medianScores) # Turns the median scores list into a string
+        partialString = intialString.replace("[", "") # Removes the left bracket from the previously defined string
+        medianScoresString = partialString.replace("]", "") # Removes the right bracket from the above string
+        print(f"Median Scores:\t\t{medianScoresString}") # Prints the new string without the brackets
 
-def main():
-    display_welcome()
-    scoreList = get_scores()
-    process_scores(scoreList)
-    print("")
-    print("Bye!")
+def main(): # Defines a function to run when the program starts 
+    display_welcome() # Calls the function display the welcome message
+    scoreList = get_scores() # Stores a list of the scores from the user
+    process_scores(scoreList) # Passes that list of scores to the process scores function for processing
+    print("\nBye!") # Prints a exit message
 
-# if started as the main module, call the main function
-if __name__ == "__main__":
-    main()
-
-
+if __name__ == "__main__": # If the program is run standalone run the following
+    main() # Calls the main function
--- MO8/Participation/AndersonKory_test_scores-3EXTRA.py
+++ MO8/Participation/AndersonKory_test_scores-3EXTRA.py
@@ -1,99 +1,99 @@
-import tkinter as tk
-from tkinter import ttk, messagebox
+import tkinter as tk # Imports a basic gui application library 
+from tkinter import ttk, messagebox # Imports some functions from that previously imported library
+ 
+class testScoresFrames(ttk.Frame): # Defines the main frame that will house the entire program
+    def __init__(self, parent): # Tells the frame what to do when initializing
+        ttk.Frame.__init__(self, parent) # Calls the initializtion function for a frame
 
-class testScoresFrames(ttk.Frame):
-    def __init__(self, parent):
-        ttk.Frame.__init__(self, parent)
+        testScoresFrame(parent).grid(row=0, column=0) # Creates a frame that houses the program
 
-        testScoresFrame(parent).grid(row=0, column=0)
+        ttk.Button(parent, text="Exit", command=parent.destroy).grid(column=1, row =2, sticky=tk.E, padx=15, pady=10) # Creates an exit button
 
-        ttk.Button(parent, text="Exit", command=parent.destroy).grid(column=1, row =2, sticky=tk.E, padx=15, pady=10)
+class testScoresFrame(ttk.Frame): # Defines the frame that houses the application
+    def __init__(self, parent): # Tells the frame what to do when initializing
+        ttk.Frame.__init__(self, parent) # Calls the initialization function for a frame
+        self.parent = parent # Stores that the parent of this frame is the parent that is passed from what called this
+        self.message = "" # Initializes the error message variable
 
-class testScoresFrame(ttk.Frame):
-    def __init__(self, parent):
-        ttk.Frame.__init__(self, parent)
-        self.parent = parent
-        self.message = ""
+        self.testScore = [tk.StringVar()] # Intializes the test score variable as an array
+        self.length = tk.StringVar() # Intializes the length variable
+        self.avgScore = tk.StringVar() # Initializes the average score variable
+        self.minScore = tk.StringVar() # Initializes the minimum score variable
+        self.maxScore = tk.StringVar() # Initializes the maximum score variable
+        self.medianScore = tk.StringVar() # Initializes the median score variable
+        self.counter =tk.StringVar() # Initializes a counter variable
 
-        self.testScore = [tk.StringVar()]
-        self.length = tk.StringVar()
-        self.avgScore = tk.StringVar()
-        self.minScore = tk.StringVar()
-        self.maxScore = tk.StringVar()
-        self.medianScore = tk.StringVar()
-        self.counter =tk.StringVar()
-
-        self.initComponents()
+        self.initComponents() # Calls a function to create the initial gui components
     
-    def clear(self):
-        self.testScore.set("")
-        self.length.set("")
-        self.avgScore.set("")
-        self.minScore.set("")
-        self.maxScore.set("")
-        self.medianScore.set("")
-        self.counter.set("")
+    def clear(self): # Defines a function to clear all entered and displayed values
+        self.testScore.set("") # Clears the test score value
+        self.length.set("") # Clears the length value
+        self.avgScore.set("") # Clears the average score value
+        self.minScore.set("") # Clears the minimum score value
+        self.maxScore.set("") # Clears the maximum score value
+        self.medianScore.set("") # Clears the median score value
+        self.counter.set("") # Clears the counter value
     
-    def makeTestScore(self):
-        counter = self.counter.get()
-        if counter == "":
-            counter = 1
-        else:
-            counter = int(counter)
-            counter += 1
-        self.testScore.append(tk.StringVar())
-        ttk.Label(self, text="Test Score:").grid(column=0, row=counter, sticky=tk.E, padx=5, pady=3)
-        ttk.Entry(self, width=25, textvariable=self.testScore[counter]).grid(column=1, row=counter, padx=5, pady=3)
+    def makeTestScore(self): # Defines a function to add an entry for a test score
+        counter = self.counter.get() # Pulls the counter that is stored in the frame
+        if counter == "": # If there is no value in the counter
+            counter = 1 # Set the counter to one
+        else: # If there was a value in the counter,
+            counter = int(counter) # Turn the counter into a integer
+            counter += 1 # Add one to the counter
+        self.testScore.append(tk.StringVar()) # Add a new value to the test score array
+        ttk.Label(self, text="Test Score:").grid(column=0, row=counter, sticky=tk.E, padx=5, pady=3) # Create a label for the new test score on the next row down
+        ttk.Entry(self, width=25, textvariable=self.testScore[counter]).grid(column=1, row=counter, padx=5, pady=3) # Creates an entry for the user to add a test score
 
-        counter = str(counter)
-        self.counter.set(f"{counter}")
+        counter = str(counter) # Converts the counter back to a string
+        self.counter.set(f"{counter}") # Stores the counter back into the frame
 
-    def initComponents(self):
-        ttk.Label(self, text="Test Score:").grid(column=0, row=0, sticky=tk.E)
-        ttk.Entry(self, width=25, textvariable=self.testScore[0]).grid(column=1, row=0)
+    def initComponents(self): # Defines the function to initialize all the components of the gui
+        ttk.Label(self, text="Test Score:").grid(column=0, row=0, sticky=tk.E) # Creates a label for the first test score entry
+        ttk.Entry(self, width=25, textvariable=self.testScore[0]).grid(column=1, row=0) # Creates the first test score entry
 
-        ttk.Label(self, text="Amount of Scores Entered:").grid(column=2, row=0, sticky=tk.E)
-        ttk.Entry(self, width=25, textvariable=self.length).grid(column=3, row=0)
+        ttk.Label(self, text="Amount of Scores Entered:").grid(column=2, row=0, sticky=tk.E) # Creates a label for the amount of scores entered
+        ttk.Entry(self, width=25, textvariable=self.length).grid(column=3, row=0) # Creates the place that the length is displayed to the user
 
-        ttk.Label(self, text="Average Test Score:").grid(column=2, row=1, sticky=tk.E)
-        ttk.Entry(self, width=25, textvariable=self.avgScore).grid(column=3, row=1)
+        ttk.Label(self, text="Average Test Score:").grid(column=2, row=1, sticky=tk.E) # Creates a label for the average test score
+        ttk.Entry(self, width=25, textvariable=self.avgScore).grid(column=3, row=1) # Creates the place that the average test score is displayed to the user
 
-        ttk.Label(self, text="Minimum Test Score:").grid(column=2, row=2, sticky=tk.E)
-        ttk.Entry(self, width=25, textvariable=self.minScore).grid(column=3, row=2)
+        ttk.Label(self, text="Minimum Test Score:").grid(column=2, row=2, sticky=tk.E) # Creates the label for the minimum entered test score
+        ttk.Entry(self, width=25, textvariable=self.minScore).grid(column=3, row=2) # Creates the place that the minimum entered test score is displayed to the user
 
-        ttk.Label(self, text="Maximum Test Score:").grid(column=2, row=3, sticky=tk.E)
-        ttk.Entry(self, width=25, textvariable=self.maxScore).grid(column=3, row=3)
+        ttk.Label(self, text="Maximum Test Score:").grid(column=2, row=3, sticky=tk.E) # Creates the label for the maximum entered test score
+        ttk.Entry(self, width=25, textvariable=self.maxScore).grid(column=3, row=3) # Creates the place that the maximum score is displayed to the user
 
-        ttk.Label(self, text="Median Test Score(s):").grid(column=2, row=4, sticky=tk.E)
-        ttk.Entry(self, width=25, textvariable=self.medianScore).grid(column=3, row=4)
+        ttk.Label(self, text="Median Test Score(s):").grid(column=2, row=4, sticky=tk.E) # Creates the label for the median entered test score
+        ttk.Entry(self, width=25, textvariable=self.medianScore).grid(column=3, row=4) # Creates the place that the median test score(s) are displayed to the user
 
-        self.makeButtons()
+        self.makeButtons() # Call a function to create the nessicary buttons
 
-        for child in self.winfo_children():
-            child.grid_configure(padx=5, pady=3)
+        for child in self.winfo_children(): # Adds spacing to every item created in this function
+            child.grid_configure(padx=5, pady=3) # Adds the spacing to the afore mentioned items
     
-    def makeButtons(self):
-        buttonFrame = ttk.Frame(self)
+    def makeButtons(self): # Defines the function to create the nessicary buttons for the program
+        buttonFrame = ttk.Frame(self) # Creates a standard frame for the button
 
-        buttonFrame.grid(column=2, row=5, columnspan=2, sticky=tk.E)
+        buttonFrame.grid(column=2, row=5, columnspan=2, sticky=tk.E) # Places the buttons in a standard location
 
-        ttk.Button(buttonFrame, text="Clear", command=self.clear).grid(column=0, row=0, padx=5)
-        ttk.Button(buttonFrame, text="Calculate", command=self.calculate).grid(column=1, row=0, padx=5)
-        ttk.Button(buttonFrame, text="Add Test Score", command=self.makeTestScore).grid(column=2, row=0)
+        ttk.Button(buttonFrame, text="Clear", command=self.clear).grid(column=0, row=0, padx=5) # Adds the clear button
+        ttk.Button(buttonFrame, text="Calculate", command=self.calculate).grid(column=1, row=0, padx=5) # Adds the calculate button
+        ttk.Button(buttonFrame, text="Add Test Score", command=self.makeTestScore).grid(column=2, row=0) # Adds the button to make a new entry for the test scores
     
-    def getInt(self, val, fieldName):
-        try:
-            return int(val)
-        except ValueError:
-            self.message += f"{fieldName} must be a valid integer"
+    def getInt(self, val, fieldName): # Defines a function to verify integers
+        try: # When ran try this code
+            return int(val) # Return the passed value as an integer
+        except ValueError: # If there was an error when running the above code, run the following
+            self.message += f"{fieldName} must be a valid integer" # Adds an error message to be displayed
     
-    def calculate(self):
-        self.message = ""
+    def calculate(self): # Defines a function to calculate and process the test scores
+        self.message = "" # Re-initialize the error message variable
 
-        testScores = []
-        counter = int(self.counter.get())
-        for i in range(0, counter + 1):
-            testScores.append(self.getInt(self.testScore[i].get(), "Test Score"))
+        testScores = [] # Create an array variable for the test scores
+        counter = int(self.counter.get()) # Pull the counter from the parent frame
+        for i in range(0, counter + 1): # For every value in the counter, run the following
+            testScores.append(self.getInt(self.testScore[i].get(), "Test Score")) # Add the test score from the entrys, into a new array
 
         if self.message == "":
             length = len(testScores)
